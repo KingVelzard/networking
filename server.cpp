e8af651c1564fd01efed7dc1497cb6e9b6e9b6b7
KingVelzard <wickeland@gmail.com>
2026-02-16 01:21:37 -0800

    added epoll multiplex I/O

--- server.cpp
+++ server.cpp
@@ -11,6 +11,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/wait.h>
@@ -19,9 +20,29 @@
 #include <poll.h>
 #include <fcntl.h>
 #include <sys/epoll.h>
+#include <unordered_map>
+#include <string>
 
 #define PORT "3490" // the port the users will be connecting to
-#define BACKLOG 1024 // how many pending connections
+#define BACKLOG 8192 // how many pending connections
+
+static const char RESPONSE[] =
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Length: 13\r\n"
+    "Connection: keep-alive\r\n"
+    "\r\n"
+    "Hello, world!";
+static const size_t RESPONSE_LEN = sizeof(RESPONSE) - 1;
+
+struct Connection {
+    int fd;
+    size_t write_offset;
+
+    Connection(int f, size_t wo)
+        : fd(f), write_offset(wo) {}
+};
+
+std::unordered_map<int, Connection> fd_map;
 
 /*
  * Convert socket to IP address string
@@ -72,128 +93,149 @@ void* get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void add_to_pfds(struct pollfd **pfds, int newfd, int *fd_count,
-		int *fd_size)
-{
-	// If we don't have room, add more space in the pfds array
-	if (*fd_count == *fd_size) {
-		*fd_size *= 2; // Double it
-		*pfds = static_cast<struct pollfd*>(
-                realloc(*pfds, sizeof(**pfds) * (*fd_size)));
-	}
-
-	(*pfds)[*fd_count].fd = newfd;
-	(*pfds)[*fd_count].events = POLLIN; // Check ready-to-read
-	(*pfds)[*fd_count].revents = 0;
-
-	(*fd_count)++;
-}
 
 
-void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
-{
-	// copy the one from the end over this one
-	pfds[i] = pfds[*fd_count - 1];
-	
-	(*fd_count)--;
-}
 
-void handle_new_connection(int listen_sockfd, int *fd_count,
-		int *fd_size, struct pollfd **pfds)
+void handle_new_connection(int listen_sockfd,
+		int epollfd, struct epoll_event* ev)
 {
 	struct sockaddr_storage remoteaddr; // Client address
 	socklen_t addrlen;
 	int newfd; // newly accepted sockfd
 	char remoteIP[INET6_ADDRSTRLEN];
 	addrlen = sizeof remoteaddr;
-	newfd = accept(listen_sockfd, (struct sockaddr*)&remoteaddr,
-			&addrlen);
-    int flags = fcntl(newfd, F_GETFL, 0);
-    fcntl(newfd, F_SETFL, flags | O_NONBLOCK);
 
-	if (newfd == -1) {
-		perror("accept");
-	} else {
-		add_to_pfds(pfds, newfd, fd_count, fd_size);
+    while (true) {
+        newfd = accept(listen_sockfd, (struct sockaddr*)&remoteaddr,
+                &addrlen);
 
-		printf("pollserver: new connection from %s on socket %d\n",
-				inet_ntop2(&remoteaddr, remoteIP, sizeof remoteIP),
-				newfd);
-		}
+
+        if (newfd == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) { 
+                break; 
+            } else { 
+                perror("accept");
+                break;
+            }
+        } else {
+            // Add to interest list of epoll
+            int flags = fcntl(newfd, F_GETFL, 0);
+            fcntl(newfd, F_SETFL, flags | O_NONBLOCK);
+            
+            int enable = 1;
+            setsockopt(newfd, IPPROTO_TCP, TCP_NODELAY, &enable, sizeof(enable));
+
+            fd_map.emplace(newfd, Connection(newfd, 0));
+
+            ev->events = EPOLLIN;
+            ev->data.fd = newfd;
+
+            if (epoll_ctl(epollfd, EPOLL_CTL_ADD, newfd, ev) == -1) {
+                perror("epoll_ct1: newfd");
+                exit(EXIT_FAILURE);
+            }
+
+            printf("pollserver: new connection from %s on socket %d\n",
+                    inet_ntop2(&remoteaddr, remoteIP, sizeof remoteIP),
+                    newfd);
+        }
+    }
 }
 
-void handle_client_data(int listen_sockfd, int *fd_count,
-		struct pollfd *pfds, int *pfd_i)
+void handle_client_data(int listen_sockfd, int epollfd,
+		struct epoll_event* events, int nfd_i)
 {
-	char buf[256];	// buffer for client data
+	char buf[4096];	// buffer for client data
 	
-	int nbytes = recv(pfds[*pfd_i].fd, buf, sizeof buf, 0);
+  	int sender_fd = events[nfd_i].data.fd;
+    
+    auto it = fd_map.find(sender_fd);
+    if (it == fd_map.end()) {
+        return;
+    }
+    Connection* conn = &(it->second);
 
-	int sender_fd = pfds[*pfd_i].fd;
 
-	const char* response =
-        	"HTTP/1.1 200 OK\r\n"
-                "Content-Length: 13\r\n"
-                "Connection: keep-alive\r\n"
-                "\r\n"
-                "Hello, world!";
+    while (true) {
+    	int nbytes = recv(sender_fd, buf, sizeof buf, 0);
+        
+        if (nbytes > 0) { // got data
+            
+            size_t total_sent = 0;
 
-	if (nbytes <= 0) { // Got error or connection closed
-		if (nbytes == 0) {
-			printf("pollserver: socket %d hung up \n", sender_fd);
+            while (total_sent < RESPONSE_LEN) {
+                int send_bytes = send(
+                    sender_fd, 
+                    RESPONSE + total_sent, 
+                    RESPONSE_LEN - total_sent, 
+                    0);
 			
-		} else {
-            perror("recv");
-		}
-		
-        close(pfds[*pfd_i].fd); 
+		        if (send_bytes == -1) {
+                    if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                        conn->write_offset = total_sent;
+                        break;
+                    } else {
+                        perror("send");
+                        epoll_ctl(epollfd, EPOLL_CTL_DEL, sender_fd, NULL);
+                        fd_map.erase(sender_fd);
+                        close(sender_fd);
+                        return;
+                    }
+                }
 
-		del_from_pfds(pfds, *pfd_i, fd_count);
+                if (send_bytes > 0) {
+                    total_sent += send_bytes;
+                }
 
-		// reexamine slot we just deleted
-		//
-		(*pfd_i)--;
-	} else { // We got good data from client
-				
-        if (send(sender_fd, response, strlen(response), 0) == -1) {
-			perror("send");
-		}
-	}
+            }
+
+            conn->write_offset = 0; // reset after complete send
+                
+        } else if (nbytes == 0) { // Connection closed
+			printf("pollserver: socket %d hung up \n", sender_fd);
+		    epoll_ctl(epollfd, EPOLL_CTL_DEL, sender_fd, NULL);
+            fd_map.erase(sender_fd);      
+            close(sender_fd); 
+            break;
+        } else {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                // no more data right now
+                break;
+		    } else {
+                perror("recv");
+                epoll_ctl(epollfd, EPOLL_CTL_DEL, sender_fd, NULL);
+                fd_map.erase(sender_fd);
+                close(sender_fd); 
+                break;
+            }
 		
+        }
+    }
 }
 
 
-void process_connections(int listen_sockfd, int *fd_count, int *fd_size,
-		struct pollfd **pfds)
+void process_connections(int listen_sockfd, int nfds, int epollfd,
+		struct epoll_event* events, struct epoll_event* ev)
 {
-	for (int i{0}; i < *fd_count; ++i)
+	for (int i{0}; i < nfds; ++i)
 	{
-		// Check if someone's ready to send
-		if ((*pfds)[i].revents & (POLLIN | POLLHUP)) {
-			// We have an event!!
-
-			if ((*pfds)[i].fd == listen_sockfd)
+			if (events[i].data.fd == listen_sockfd)
 			{
 				// if its the listener, new connection
-				handle_new_connection(listen_sockfd, fd_count, fd_size,
-							pfds);
+				handle_new_connection(listen_sockfd, epollfd, ev);
 			} else {
 				// otherwise it's a regular client request
-				handle_client_data(listen_sockfd, fd_count, *pfds, &i);
+				handle_client_data(listen_sockfd, epollfd, events, i);
 			}
-		}
 	}
 }
 
 int main(void)
 {
-	int listen_sockfd, recv_sockfd;
-       	struct addrinfo hints, *servinfo, *p;
-	struct sockaddr_storage their_addr; // connector addr info
-	socklen_t sin_size;
+	int listen_sockfd;
+    struct addrinfo hints, *servinfo, *p;
 	struct sigaction sa;
 	int yes=1;
-	char s[INET6_ADDRSTRLEN];
 	int rv;
 	thread_pool::ThreadPool my_pool{
 		static_cast<size_t>(std::thread::hardware_concurrency()
@@ -205,10 +247,11 @@ int main(void)
 	hints.ai_flags = AI_PASSIVE; // use my ip
 	
 	// polling inits
-	int fd_size = 100;
-	int fd_count = 1;
-	struct pollfd *pfds = static_cast<struct pollfd*>(
-            malloc(sizeof *pfds * fd_size));	
+    struct epoll_event ev, events[BACKLOG];
+    int epollfd;
+    int nfds;
+    
+    fd_map.reserve(10000);
 
 	if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -258,24 +301,37 @@ int main(void)
 		perror("sigaction");
 		exit(1);
 	}
-	
-	pfds[0].fd = listen_sockfd;
-	pfds[0].events = POLLIN;
+    
+    int flags = fcntl(listen_sockfd, F_GETFL, 0);
+    fcntl(listen_sockfd, F_SETFL, flags | O_NONBLOCK);
+
+    epollfd = epoll_create1(0);
+    if (epollfd == -1) {
+        perror("epoll_create1");
+        exit(EXIT_FAILURE);
+    }
+
+    ev.events = EPOLLIN | EPOLLET;
+    ev.data.fd = listen_sockfd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sockfd, &ev) == -1) {
+        perror("epoll_ctl: listen_sockfd");
+        exit(EXIT_FAILURE);
+    }
 	printf("server: waiting for connections...\n");
 
 
 
 	while (1) { // main accept loop
-		int poll_count = poll(pfds, fd_count, -1);
+		nfds = epoll_wait(epollfd, events, BACKLOG, -1);
 
-		if (poll_count == -1) {
-			perror("poll");
-			exit(1);
-		}
+        if (nfds == -1) {
+            perror("epoll_wait");
+            exit(EXIT_FAILURE);
+        }
 
-		process_connections(listen_sockfd, &fd_count, &fd_size, &pfds);
+		process_connections(listen_sockfd, nfds, epollfd, events, &ev);
 	}
-	
-	free(pfds);
+    
 	return 0;
 }
+
